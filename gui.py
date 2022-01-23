acd0d30047e40abc3b141878bd783119b8262adb
Edric Antoine <edric.antoine@gmail.com>
2022-01-22 18:39:46 -0800

    Can now sort by gains and losses

--- gui.py
+++ gui.py
@@ -2,7 +2,7 @@ import codecs
 import tkinter as tk
 import csv
 import tkinter.filedialog
-import main
+import tkinter.ttk as ttk
 
 window = tk.Tk()
 window.winfo_toplevel().title("pyCSV Reader for RBC statements")
@@ -12,29 +12,134 @@ mainLabel = tk.Label(text='To view a .csv file, click the "Open CSV" button.', b
 mainLabel.pack()
 
 net_label = None
+rows = []
 
 fileOpened = False
-fileReader = main.csvReader
 
 
-def putStatsToGui(rows):
+def reactToEntry():
+    global net_label
+    text = commandEntry.get()
+    match text:
+        case "gain":
+
+            temp = 0
+            count = 0
+            for row in rows:
+                if float(row[6]) * 100 > 0:
+                    temp += float(row[6]) * 100
+                    count += 1
+            temp = temp / 100
+            if net_label is None:
+                net_label = tk.Label(
+                    text='You made ' + str(count) + ' gains over this period of time.\nThese total to $' + str(
+                        temp) + ".",
+                    bg="#CBCBCB", fg="black")
+            else:
+                net_label.config(
+                    text='You made ' + str(count) + ' gains over this period of time.\nThese total to $' + str(
+                        temp) + ".",
+                    bg="#CBCBCB", fg="black")
+
+        case "loss":
+
+            temp = 0
+            count = 0
+            for row in rows:
+                if float(row[6]) * 100 < 0:
+                    temp += float(row[6]) * 100
+                    count += 1
+            temp = temp / 100
+            if net_label is None:
+                net_label = tk.Label(
+                    text='You made ' + str(count) + ' losses over this period of time.\nThese total to $' + str(
+                        temp) + ".",
+                    bg="#CBCBCB", fg="black")
+            else:
+                net_label.config(
+                    text='You made ' + str(count) + ' losses over this period of time.\nThese total to $' + str(
+                        temp) + ".",
+                    bg="#CBCBCB", fg="black")
+
+        case "none":
+            temp = 0
+            count = 0
+            for row in rows:
+                temp += float(row[6]) * 100
+                count += 1
+            temp = temp / 100
+            if net_label is None:
+                net_label = tk.Label(
+                    text='You made ' + str(
+                        count) + ' transactions over this period of time.\nYour net gain/loss was $' + str(
+                        temp) + ". \n (A negative number "
+                                "means a net loss.)",
+                    bg="#CBCBCB", fg="black")
+            else:
+                net_label.config(
+                    text='You made ' + str(
+                        count) + ' transactions over this period of time.\nYour net gain/loss was $' + str(
+                        temp) + ". \n (A negative number "
+                                "means a net loss.)",
+                    bg="#CBCBCB", fg="black")
+
+        case "place":
+            openDropdownPicker(text)
+        case "date":
+            openDropdownPicker(text)
+        case "type":
+            openDropdownPicker(text)
+
+
+def openDropdownPicker(cat):
+    new_window = tk.Toplevel(window)
+    new_window.title = "Select category"
+    choices = []
+    match cat:
+        case "place":
+            for row in rows:
+                choices.append(row[5])
+        case "date":
+            for row in rows:
+                choices.append(row[2])
+        case "type":
+            for row in rows:
+                choices.append(row[4])
+
+    chosen_button = tk.Button(
+        text="Go",
+        width=5,
+        height=2,
+        fg="black",
+        bg="#8A8989",
+    )
+
+
+def putStatsToGui():
     global net_label
     temp = 0
+    count = 0
     for row in rows:
         temp += float(row[6]) * 100
+        count += 1
     temp = temp / 100
     if net_label is None:
         net_label = tk.Label(
-            text='Your net gain over this period of time was $' + str(temp) + ". \n (A negative number "
-                                                                              "means a net loss.)",
+            text='You made ' + str(count) + ' transactions over this period of time.\nYour net gain/loss was $' + str(
+                temp) + ". \n (A negative number "
+                        "means a net loss.)",
             bg="#CBCBCB", fg="black")
     else:
         net_label.config(
-            text='Your net gain over this period of time was $' + str(temp) + ". \n (A negative number "
-                                                                              "means a net loss.)",
+            text='You made ' + str(count) + ' transactions over this period of time.\nYour net gain/loss was $' + str(
+                temp) + ". \n (A negative number "
+                        "means a net loss.)",
             bg="#CBCBCB", fg="black")
 
     net_label.pack()
+    commandEntry.pack()
+    sortButton.pack()
+    tipLabel.pack()
     csvButton.config(text="Open new CSV",
                      width=8,
                      height=2,
@@ -46,18 +151,18 @@ def putStatsToGui(rows):
 
 
 def displayStats(creader):
+    global rows
     header = next(creader)
     rows = []
     for row in creader:
         rows.append(row)
-    putStatsToGui(rows)
+    putStatsToGui()
 
 
 def openFilePicker():
     global fileOpened
     name = tkinter.filedialog.askopenfilename(title="Select CSV file", filetypes=(("CSV files", "*.csv"),))
     filename = codecs.open(name, encoding='latin-1')
-    fileReader.file = filename
     csvreader = csv.reader(filename, )
     fileOpened = True
     displayStats(csvreader)
@@ -72,6 +177,20 @@ csvButton = tk.Button(
     bg="#8A8989",
     command=openFilePicker
 )
+
+commandEntry = tk.Entry()
+commandEntry.insert("end", "<insert command here>")
+
+sortButton = tk.Button(
+    text="Sort by...",
+    width=5,
+    height=2,
+    fg="black",
+    bg="#8A8989",
+    command=reactToEntry
+)
+
+tipLabel = tk.Label(text='(Sort by "place", "date", "type", "gain", "loss", or "none".)', bg="#CBCBCB", fg="black")
 
 csvButton.pack()
 
--- main.py
+++
@@ -1,5 +0,0 @@
-import csv
-
-
-class csvReader:
-    file = None
